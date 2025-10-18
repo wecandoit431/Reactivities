cdf882a7292b3398d2a4f16058958880264b243a
Sudhajit <deysudhajit@gmail.com>
2025-10-19 01:00:43 +0530

    End of section 2

---
+++ Persistance/AppDbContext.cs
@@ -0,0 +1,13 @@
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+
+        public DbSet<Activity> Activities { get; set; }
+      
+
+    }
+}
